52b90089cc2faf791e54d4c149383ee85c9897d6
gillchristian <gillchristiang@gmail.com>
2018-12-02 12:26:39 +0100

    Day 2

---
+++ 2018/02-01.hs
@@ -0,0 +1,21 @@
+module Main where
+
+import Data.List (sort, elemIndex)
+import Data.List.Unique (count)
+
+main :: IO ()
+main = do
+  s <- readFile "02-01.input.txt"
+  let t = foldl search (0, 0) $ lines s
+  print t
+  print $ fst t * snd t -- result
+
+search :: (Integer, Integer) -> String -> (Integer, Integer)
+search (c2, c3) line = (c2', c3')
+  where tc = map snd . count $ line
+        c2' = case elemIndex 2 tc of
+                Just _ -> c2 + 1
+                Nothing -> c2
+        c3' = case elemIndex 3 tc of
+                Just _ -> c3 + 1
+                Nothing -> c3
---
+++ 2018/02-02.hs
@@ -0,0 +1,34 @@
+module Main where
+
+import Data.List (find)
+import Data.Maybe
+import Data.List.Utils (replace)
+
+main :: IO ()
+main = do
+  s <- readFile "02-01.input.txt"
+  let ls = lines s
+  let (a, b) = case foldl (search ls) Nothing ls of
+                Just r -> r
+                Nothing -> ("Not", "found")
+  putStrLn $ removeDiffByPos a b
+
+search :: [String] -> Maybe (String, String) -> String -> Maybe (String, String)
+search _ (Just r) _ = Just r
+search ls Nothing s = case find (diffByOne s) ls of
+                        Just r -> Just (s, r)
+                        Nothing -> Nothing
+
+diffByOne :: String -> String -> Bool
+diffByOne a b = (==1) $ sum $ map tplDiff $ zip a b
+
+removeDiffByPos :: String -> String -> String
+removeDiffByPos a b = fst $ unzip $ filter tplEq $ zip a b
+
+tplDiff :: (Eq a) => (a,a) -> Integer
+tplDiff (a, b) 
+  | a == b = 0
+  | otherwise = 1
+
+tplEq :: (Eq a) => (a,a) -> Bool
+tplEq (a, b) = a == b 
