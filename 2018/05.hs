9d1ff61e7273435e169aba7014854c4242c9e48b
gillchristian <gillchristiang@gmail.com>
2018-12-05 20:39:00 +0100

    Day 5 completed

---
+++ 2018/05.hs
@@ -0,0 +1,30 @@
+module Main where
+
+import           Data.Char
+import           Data.Set  (fromList, toList)
+
+main :: IO ()
+main = do
+  input <- readFile "05.input.txt"
+  let clean = filter isLetter input
+  print $ collapse clean
+  print $ findBest clean
+
+collapse :: String -> Int
+collapse = go ""
+  where
+    go :: String -> String -> Int
+    go [] (n:ns) = go [n] ns
+    go (a:as) (b:bs)
+      | a /= b && toLower a == toLower b = go as bs
+      | otherwise = go (b : a : as) bs
+    go collapsed [] = length collapsed
+
+findBest :: String -> Int
+findBest s =
+  minimum .
+  map (\c -> collapse . filter ((c /=) . toLower) $ s) . unique . map toLower $
+  s
+
+unique :: (Ord a) => [a] -> [a]
+unique = toList . fromList
