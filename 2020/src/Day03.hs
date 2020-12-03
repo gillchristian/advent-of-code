389677798a456cc3e81f22ec13e39d59701a8b7a
gillchristian <gillchristiang@gmail.com>
2020-12-03 09:32:10 +0100

    feat(2020): Day 03 - Part 2

--- 2020/src/Day03.hs
+++ 2020/src/Day03.hs
@@ -2,24 +2,34 @@ module Day03 where
 
 type Coord = (Int, Int)
 
-mkCoordinates :: Coord -> [Coord]
-mkCoordinates (h, w) = accer (h - 1) (0, 0)
-  where
-    step (y, x) = (y + 1, (x + 3) `mod` w)
-    accer 1 coord = [step coord]
-    accer n coord = step coord : accer (n - 1) (step coord)
+data Strategy = Strategy {right :: Int, down :: Int}
 
-counter :: [String] -> (Int, Int) -> Coord -> (Int, Int)
-counter grid (trees, empties) (y, x) =
-  case (grid !! y) !! x of
-    '.' -> (trees, empties + 1)
-    '#' -> (trees + 1, empties)
-    c -> error $ "Got unexpected char '" <> [c] <> "'"
+mkCoords :: Strategy -> Coord -> [Coord]
+mkCoords strategy (h, w) = accer (h - 1) (0, 0)
+  where
+    step (y, x) = (y + down strategy, (x + right strategy) `mod` w)
+    accer n coord
+      | n <= 0 = []
+      | otherwise = step coord : accer (n - down strategy) (step coord)
+
+countTree :: [String] -> Int -> Coord -> Int
+countTree grid trees (y, x)
+  | grid !! y !! x == '#' = trees + 1
+  | otherwise = trees
+
+solve :: [String] -> Strategy -> Int
+solve grid strategy =
+  foldl (countTree grid) 0 $ mkCoords strategy (length grid, length $ head grid)
 
 day03 :: IO ()
 day03 = do
   input <- lines <$> readFile "input/day03.txt"
-  let h = length input
-      w = length $ head input
-      (trees, _) = foldl (counter input) (0, 0) $ mkCoordinates (h, w)
-  putStrLn $ "Part 1: " <> show trees <> " trees"
+  putStrLn $ "Part 1: " <> show (solve input Strategy {down = 1, right = 3}) <> " trees"
+  let strategiesP2 =
+        [ Strategy {down = 1, right = 1},
+          Strategy {down = 1, right = 3},
+          Strategy {down = 1, right = 5},
+          Strategy {down = 1, right = 7},
+          Strategy {down = 2, right = 1}
+        ]
+  putStrLn $ ("Part 2: " <>) $ show $ product $ map (solve input) strategiesP2
