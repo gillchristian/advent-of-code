ddc7dc6c49838e7d17bed277fcb9db72077020c3
gillchristian <gillchristiang@gmail.com>
2020-12-05 20:43:15 +0100

    feat(2020): Improve Day 05

--- 2020/src/Day05.hs
+++ 2020/src/Day05.hs
@@ -1,18 +1,15 @@
 module Day05 where
 
 import Data.List (find, sort)
+import Numeric
 
 toBinary :: Char -> Int
-toBinary 'F' = 0
-toBinary 'B' = 1
-toBinary 'L' = 0
-toBinary 'R' = 1
+toBinary c 
+  | c == 'F' || c == 'L' = 0
+  | c == 'B' || c == 'R' = 1
 
-seatId :: [Int] -> Int
-seatId [r1, r2, r3, r4, r5, r6, r7, c1, c2, c3] = r * 8 + c
-  where
-    r = r1 * 64 + r2 * 32 + r3 * 16 + r4 * 8 + r5 * 4 + r6 * 2 + r7
-    c = c1 * 4 + c2 * 2 + c3
+readSeatId :: String -> Int
+readSeatId = fst . head . readInt 2 (const True) toBinary
 
 findMissingSeat :: [Int] -> Maybe Int
 findMissingSeat ids = fmap snd $ find (uncurry (/=)) $ zip ids [head ids ..]
@@ -20,7 +17,7 @@ findMissingSeat ids = fmap snd $ find (uncurry (/=)) $ zip ids [head ids ..]
 day05 :: IO ()
 day05 = do
   input <- lines <$> readFile "input/day05.txt"
-  let ids = sort $ fmap (seatId . fmap toBinary) input
+  let ids = sort $ fmap readSeatId input
   putStrLn $ "Part 01: the highest is #" <> show (last ids)
   let (Just missing) = findMissingSeat ids
   putStrLn $ "Part 02: the missing is #" <> show missing
