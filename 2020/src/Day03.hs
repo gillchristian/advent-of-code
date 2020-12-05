7046c95ac4a7884c090975d58fe291b6bb56b1e4
gillchristian <gillchristiang@gmail.com>
2020-12-05 21:54:11 +0100

    feat(2020): Improve Day 03

--- 2020/src/Day03.hs
+++ 2020/src/Day03.hs
@@ -4,30 +4,31 @@ type Coord = (Int, Int)
 
 data Strategy = Strategy {right :: Int, down :: Int}
 
-mkCoords :: Strategy -> Coord -> [Coord]
-mkCoords strategy (h, w) = accer (h - 1) (0, 0)
+mkCoords :: Strategy -> Int -> [Coord]
+mkCoords strategy h = go (h - 1) (0, 0)
   where
-    step (y, x) = (y + down strategy, (x + right strategy) `mod` w)
-    accer n coord
+    step (y, x) = (y + down strategy, x + right strategy)
+    go n coord
       | n <= 0 = []
-      | otherwise = step coord : accer (n - down strategy) (step coord)
+      | otherwise = step coord : go (n - down strategy) (step coord)
 
 countTree :: [String] -> Int -> Coord -> Int
-countTree grid trees (y, x)
-  | grid !! y !! x == '#' = trees + 1
-  | otherwise = trees
+countTree grid count (y, x)
+  | grid !! y !! x == '#' = count + 1
+  | otherwise = count
 
 solve :: [String] -> Strategy -> Int
 solve grid strategy =
-  foldl (countTree grid) 0 $ mkCoords strategy (length grid, length $ head grid)
+  foldl (countTree grid) 0 $ mkCoords strategy $ length grid
 
 day03 :: IO ()
 day03 = do
-  input <- lines <$> readFile "input/day03.txt"
-  putStrLn $ "Part 1: " <> show (solve input Strategy {down = 1, right = 3}) <> " trees"
+  input <- fmap cycle . lines <$> readFile "input/day03.txt"
+  let strategy1 = Strategy {down = 1, right = 3}
+  putStrLn $ "Part 1: " <> show (solve input strategy1) <> " trees"
   let strategiesP2 =
         [ Strategy {down = 1, right = 1},
-          Strategy {down = 1, right = 3},
+          strategy1,
           Strategy {down = 1, right = 5},
           Strategy {down = 1, right = 7},
           Strategy {down = 2, right = 1}
