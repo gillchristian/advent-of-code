2f54e42cc65557ffa698bc800cd5fda4ed4729c8
gillchristian <gillchristiang@gmail.com>
2021-12-01 13:10:22 -0300

    support(2021): Add other solution

--- 2021/src/Day01.hs
+++ 2021/src/Day01.hs
@@ -17,3 +17,23 @@ day01 = do
   ls :: [Int] <- fmap read . lines <$> readFile "input/day01.txt"
   print $ countIncreases 1 ls
   print $ countIncreases 3 ls
+
+-- Solution by [and-pete](https://github.com/and-pete)
+--
+-- Compares just the non-overlapping elements
+-- of the two sliding windows of Ints. e.g. in
+-- the below sample
+--   199  A      
+--   200  A B    
+--   208  A B
+--   210    B 
+-- ..the 200 and 208 are common to windows A + B
+-- so you can just compare 210 to 199
+solveApartN :: Int -> [Int] -> Int
+solveApartN n xs =
+    sum $ zipWith scoreIncrease xs (drop n xs)
+  where
+  scoreIncrease :: Int -> Int -> Int
+  scoreIncrease old new
+    | new > old = 1
+    | otherwise = 0
