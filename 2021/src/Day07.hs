4c2b89eae21f695bedb4aabbe38837c07fa842f6
gillchristian <gillchristiang@gmail.com>
2021-12-07 16:07:15 -0300

    support(2021): Improve Day 07

--- 2021/src/Day07.hs
+++ 2021/src/Day07.hs
@@ -3,6 +3,15 @@ module Day07 where
 import Data.List (nub, sort)
 import Data.List.Split (splitOn)
 
+median :: (Ord a, Fractional a) => [a] -> a
+median = fromSorted . sort
+  where
+  fromSorted :: Fractional a => [a] -> a
+  fromSorted [] = error "Empty list"
+  fromSorted [a] = a
+  fromSorted [a,b] = ((a + b) / 2)
+  fromSorted (a:xs) = fromSorted (init xs)
+
 parse :: String -> [Int]
 parse = fmap read . splitOn ","
 
@@ -10,18 +19,25 @@ diff :: Num a => a -> a -> a
 diff a b = abs $ a - b
 
 sumToN :: Int -> Int
-sumToN n = floor (fromIntegral (n * (n + 1)) / 2)
+sumToN n = (n * (n + 1)) `quot` 2
 
-solve :: (Int -> Int -> Int) -> [Int] -> Int
-solve f ns = 
+part1 :: [Int] -> Int
+part1 ns = sum $ fmap (diff m) ns
+  where
+  m = floor $ median $ fmap fromIntegral ns
+
+part2 :: [Int] -> Int
+part2 ns = 
   head 
     $ sort 
     $ fmap sum 
-    $ fmap (\x -> fmap (f x) ns) 
+      -- Naive solution, should memoize the diffs
+      -- of each number to calculate only once
+    $ fmap (\x -> fmap (sumToN . diff x) ns) 
     $ [(minimum ns)..(maximum ns)]
 
 day07 :: IO ()
 day07 = do
   ns <- parse <$> readFile "input/day07.txt"
-  print $ solve diff ns
-  print $ solve (\a b -> sumToN $ diff a b) ns
+  print $ part1 ns
+  print $ part2 ns
