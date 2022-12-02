e1d423075b1b5717bc63645ae923ebee845b0ff0
gillchristian <gillchristiang@gmail.com>
2022-12-02 10:29:39 +0100

    feat(2022): Day 02, Part 2

--- 2022/src/days/02/index.ts
+++ 2022/src/days/02/index.ts
@@ -3,7 +3,7 @@ import * as E from "fp-ts/Either";
 import * as A from "fp-ts/ReadonlyArray";
 import * as num from "fp-ts/number";
 import { pipe } from "fp-ts/function";
-import { sequenceT } from "fp-ts/Apply";
+import { sequenceT, sequenceS } from "fp-ts/Apply";
 import { match } from "ts-pattern";
 
 import { lines, words } from "../../lib/string";
@@ -20,7 +20,14 @@ const enum Self {
   Scissors = "Z",
 }
 
+const enum ExpectedResult {
+  Lose = "X",
+  Draw = "Y",
+  Win = "Z",
+}
+
 type Match = [Oponent, Self];
+type Strategy = [Oponent, ExpectedResult];
 
 const parseOponent = (c: string): E.Either<string, Oponent> =>
   match(c)
@@ -36,18 +43,27 @@ const parseSelf = (c: string): E.Either<string, Self> =>
     .with("Z", () => E.right(Self.Scissors))
     .otherwise(() => E.left(`Got invalid self "${c}"`));
 
-const parseMatch = (line: string): E.Either<string, Match> => {
+const parseExpectedResult = (c: string): E.Either<string, ExpectedResult> =>
+  match(c)
+    .with("X", () => E.right(ExpectedResult.Lose))
+    .with("Y", () => E.right(ExpectedResult.Draw))
+    .with("Z", () => E.right(ExpectedResult.Win))
+    .otherwise(() => E.left(`Got invalid strategy "${c}"`));
+
+const parseBoth = (
+  line: string
+): E.Either<string, { match: Match; strategy: Strategy }> => {
   const [o, s] = words(line.trim());
 
   if (!o || !s) {
     return E.left(`Got invalid match: "${line}"`);
   }
 
-  return sequenceT(E.Apply)(parseOponent(o), parseSelf(s));
+  return sequenceS(E.Apply)({
+    match: sequenceT(E.Apply)(parseOponent(o), parseSelf(s)),
+    strategy: sequenceT(E.Apply)(parseOponent(o), parseExpectedResult(s)),
+  });
 };
-
-// Score of Self: Rock - 1, Paper - 2, Scissors - 3
-// Plus score for the outcome: lose - 0 if you lost, draw - 3, win - 6
 
 const scoreSelf = (s: Self): number =>
   match(s)
@@ -56,15 +72,16 @@ const scoreSelf = (s: Self): number =>
     .with(Self.Scissors, () => 3)
     .exhaustive();
 
-const score = ([o, s]: Match): number =>
+const scoreMatch = ([o, s]: Match): number =>
   match([o, s])
-    // Draw = 1 + Self
+    // Draw = 3 + Self
     .with(
       [Oponent.Rock, Self.Rock],
       [Oponent.Paper, Self.Paper],
       [Oponent.Scissors, Self.Scissors],
       () => 3 + scoreSelf(s)
     )
+
     // Win  = 6 + Self
     .with(
       [Oponent.Rock, Self.Paper],
@@ -72,6 +89,7 @@ const score = ([o, s]: Match): number =>
       [Oponent.Scissors, Self.Rock],
       () => 6 + scoreSelf(s)
     )
+
     // Lose = Self
     .with(
       [Oponent.Rock, Self.Scissors],
@@ -79,19 +97,56 @@ const score = ([o, s]: Match): number =>
       [Oponent.Scissors, Self.Paper],
       () => scoreSelf(s)
     )
+
     .otherwise(() => 0);
 
-const parse = (input: string): E.Either<string, readonly Match[]> =>
-  pipe(input.trim(), lines, E.traverseArray(parseMatch));
+const playStrategy = ([o, s]: Strategy): number =>
+  match([s, o])
+
+    // Draw = 3 + Self
+    .with([ExpectedResult.Draw, Oponent.Rock], () => 3 + scoreSelf(Self.Rock))
+    .with([ExpectedResult.Draw, Oponent.Paper], () => 3 + scoreSelf(Self.Paper))
+    .with([ExpectedResult.Draw, Oponent.Scissors], () => 3 + scoreSelf(Self.Scissors))
+
+    // Win  = 6 + Self
+    .with([ExpectedResult.Win, Oponent.Rock], () => 6 + scoreSelf(Self.Paper))
+    .with([ExpectedResult.Win, Oponent.Paper], () => 6 + scoreSelf(Self.Scissors))
+    .with([ExpectedResult.Win, Oponent.Scissors], () => 6 + scoreSelf(Self.Rock))
+
+    // Lose = Self
+    .with([ExpectedResult.Lose, Oponent.Rock], () => scoreSelf(Self.Scissors))
+    .with([ExpectedResult.Lose, Oponent.Paper], () => scoreSelf(Self.Rock))
+    .with([ExpectedResult.Lose, Oponent.Scissors], () => scoreSelf(Self.Paper))
+
+    .otherwise(() => 0);
+
+const parse = (
+  input: string
+): E.Either<string, readonly { match: Match; strategy: Strategy }[]> =>
+  pipe(input.trim(), lines, E.traverseArray(parseBoth));
 
 const INPUT_FILE = "src/days/02/input.txt";
 
 fs.readFile(INPUT_FILE, "utf-8")
   .then(parse)
-  .then(E.map(A.foldMap(num.MonoidSum)(score)))
+  .then(
+    E.map((xs): [number, number] => [
+      pipe(
+        xs,
+        A.foldMap(num.MonoidSum)((x) => scoreMatch(x.match))
+      ),
+      pipe(
+        xs,
+        A.foldMap(num.MonoidSum)((x) => playStrategy(x.strategy))
+      ),
+    ])
+  )
   .then(
     E.match(
       (e) => console.error(`Failed to parse: ${e}`),
-      (result) => console.log(`The score is: ${result}`)
+      ([res1, res2]) => {
+        console.log(`The score of all matches:       ${res1}`);
+        console.log(`The score with the strategy is: ${res2}`);
+      }
     )
   );
